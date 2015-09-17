7bfbc09e9810803f6fe9433d4f815169ac3b70eb
ds007007 <ds007007@yandex.ru>
2015-09-17 17:09:32 +0300

    Timer time update logic

--- client/app/timer_directive/timer_directive.js
+++ client/app/timer_directive/timer_directive.js
@@ -1,11 +1,38 @@
 'use strict';
 
 angular.module('zharaApp')
-  .directive('appTimer', function () {
+  .directive('appTimer', function ($interval, dateFilter) {
     return {
       templateUrl: 'app/timer_directive/timer_directive.html',
       restrict: 'EA',
       link: function (scope, element, attrs) {
+        var timeoutId;
+
+        // start the UI update process; save the timeoutId for canceling
+        timeoutId = $interval(function(){
+          updateTime();
+        }, 1000);
+
+        element.on('$destroy', function(){
+          $interval.cancel(timeoutId);
+        });
+
+        var updateTime = function(){
+          scope.time = getTime();
+        }
+
+        var getTime = function(){
+          var time = new Date();
+
+          return {
+            d: time.getDate(),
+            h: time.getHours(),
+            m: time.getMinutes(),
+            s: time.getSeconds()
+          };
+        }
+
+        scope.time = getTime();
       }
     };
   });
