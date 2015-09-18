631781b6490d6fecfcacff0ee04db7b3a8d18a81
ds007007 <ds007007@yandex.ru>
2015-09-18 16:05:18 +0300

    Production configs

--- postInstall.js
+++ postInstall.js
@@ -3,12 +3,15 @@ require('shelljs/global');
 var is_production = process.env.NODE_ENV == "production";
 
 if (is_production) {
-  console.log('running bower install');
+  console.log('------------Production environment detected------------');
+  console.log('------------Running "bower install"------------');
   exec('node node_modules/bower/bin/bower install');
-  console.log('running grunt build:dist');
+
+  console.log('------------Running "grunt build:dist"------------');
   exec('node node_modules/grunt-cli/bin/grunt build:dist');
+
   return;
 }
 
-console.error('No postinstall task');
+console.error('------------!No postinstall task!------------');
 process.exit(0);
